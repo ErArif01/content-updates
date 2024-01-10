251b985151efd0c6e9cc75d9021159a9b6259e21
ER MOHD ARIF <141546622+ErArif01@users.noreply.github.com>
2024-01-10 16:06:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
+// script.js
+
 $(document).ready(function () {
-  function loadContent(page) {
-    $.getJSON(`data/${page}.json`, function (data) {
-      const template= $('#template'.html();
-      const compiledTemplate = Handlebars.compile(template);
-      const html = compiledTemplate(data);
-      $('#content').html(html);
-    });
-  }
+    function loadContent(page) {
+        // Reference to the Firebase database
+        var database = firebase.database().ref('/data/' + page);
 
-  const currentPage = window.location.pathname.split('/').pop().split('.')[0];
-  loadContent(currentPage);
+        // Fetch data from Firebase
+        database.once('value').then(function(snapshot) {
+            var data = snapshot.val();
 
-  $('nav a').click(function (e) {
+            // Handlebars rendering
+            const template = $('#template').html();
+            const compiledTemplate = Handlebars.compile(template);
+            const html = compiledTemplate(data);
+            $('#content').html(html);
+        });
+    }
+
+    // Initial load
+    const currentPage = window.location.pathname.split('/').pop().split('.')[0];
+    loadContent(currentPage);
+
+    // Navigation click event
+    $('nav a').click(function (e) {
         e.preventDefault();
         const page = $(this).attr('href').split('.').shift();
         loadContent(page);
-
     });
 });
